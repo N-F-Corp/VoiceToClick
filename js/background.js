0199ab8aa6b2e19e733ebb3039f6ff17595d79ee
Nik <NikBenson@pm.me>
2020-07-16 21:40:15 +0200

    added default voice storage

--- js/background.js
+++ js/background.js
@@ -1,5 +1,9 @@
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+		const voice = window.speechSynthesis.getVoices()[0];
+		chrome.storage.sync.set({voice: voice}, function() {
+			console.log('Set voice to ' + voice?.name + '!');
+		});
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [new chrome.declarativeContent.PageStateMatcher({})],
 			actions: [new chrome.declarativeContent.ShowPageAction()]
